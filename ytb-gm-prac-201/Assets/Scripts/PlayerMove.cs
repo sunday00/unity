2e3067495953445f6ec53550c9dbac09a02e2dd2
sunday00 <kind65505256@gmail.com>
2025-02-25 02:31:59 +0900

    jump

--- ytb-gm-prac-201/Assets/Scripts/PlayerMove.cs
+++ ytb-gm-prac-201/Assets/Scripts/PlayerMove.cs
@@ -7,13 +7,26 @@ public class PlayerMove : MonoBehaviour
     public Rigidbody2D rb;
     public float maxSpeed;  
 
+    public float jumpForce;
+    public int jumpCount = 0;
+    
+    public SpriteRenderer sr;
+    public Animator animator;
+    
     public void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
 
     public void Update()
     {
+        if (Input.GetButtonDown("Horizontal"))
+        {
+            this.sr.flipX = Input.GetAxisRaw("Horizontal") < 0;
+        }
+        
         if (Input.GetButtonUp("Horizontal"))
         {
             rb.linearVelocity = new Vector2(
@@ -21,22 +34,41 @@ public class PlayerMove : MonoBehaviour
                 rb.linearVelocityY
             );   
         }
+        
+        animator.SetBool("isWalking", Mathf.Abs(rb.linearVelocityX) > 0.3f);
+        
+        // jump
+
+        if (Input.GetButtonDown("Jump") || (Input.GetButtonDown("Vertical") && Input.GetAxisRaw("Vertical") > 0))
+        {
+            if (jumpCount > 2) return;
+            
+            animator.SetBool("isJumping", true);
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            jumpCount++;
+        }
     }
 
     public void FixedUpdate()
     {
-        float h = Input.GetAxis("Horizontal");  
-        rb.AddForce(Vector2.right * h, ForceMode2D.Impulse);
-
-        // if (this.rb.velocity.x > maxSpeed)
-        if (Math.Abs(rb.linearVelocityX) > maxSpeed)
+        rb.AddForce(Vector2.right * Input.GetAxis("Horizontal"), ForceMode2D.Impulse);
+        
+        if (Mathf.Abs(rb.linearVelocityX) > maxSpeed)
         {
-            rb.linearVelocity = new Vector2(maxSpeed * (rb.linearVelocityX >= 0 ? 1 : -1), rb.linearVelocityY);
+            rb.linearVelocity = new Vector2(maxSpeed * rb.linearVelocity.normalized.x, rb.linearVelocityY);
         }
-        // else if (this.rb.linearVelocityX < maxSpeed * -1)
-        // {
-        //     this.rb.linearVelocity = new Vector2(maxSpeed * -1, rb.linearVelocityY);
-        // }
+        
+        // de Jump
+        Debug.DrawRay(rb.position, Vector3.down, Color.red);
+        RaycastHit2D rayHit = Physics2D.Raycast(
+            rb.position, Vector3.down, 1, LayerMask.GetMask("Floor")
+        );
+        if (rayHit.collider != null && rayHit.distance < 0.5f && rb.linearVelocity.y < 0)
+        {
+            // print(rayHit.collider.name);
+            animator.SetBool("isJumping", false);
+            jumpCount = 0;
+        }
     }
 
     public void LateUpdate()
