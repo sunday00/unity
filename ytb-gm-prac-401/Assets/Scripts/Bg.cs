290f35a92b2564ef42b704b8e57aae04922d47af
sunday00 <kind65505256@gmail.com>
2025-03-23 02:26:24 +0900

    camera size finf

--- ytb-gm-prac-401/Assets/Scripts/Bg.cs
+++ ytb-gm-prac-401/Assets/Scripts/Bg.cs
@@ -3,11 +3,17 @@ using UnityEngine;
 public class Bg : MonoBehaviour
 {
     public float speed;
+    public float viewHeight;
+
+    private void Awake()
+    {
+        viewHeight = Camera.main.orthographicSize * 2;
+    }
 
     private void Update()
     {
-        transform.position += Vector3.down * speed * Time.deltaTime;
+        transform.position += Vector3.down * (speed * Time.deltaTime);
 
-        if (transform.position.y < -12) transform.position = Vector3.up * 18;
+        if (transform.position.y < -1.2f * viewHeight) transform.position = Vector3.up * (viewHeight * 1.8f);
     }
 }
