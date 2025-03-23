db5b2ee4ca6e78c28750dac1e3234ff1ee972022
sunday00 <kind65505256@gmail.com>
2025-03-23 17:20:54 +0900

    level design

--- ytb-gm-prac-401/Assets/Scripts/EnemySpawn.cs
+++ ytb-gm-prac-401/Assets/Scripts/EnemySpawn.cs
@@ -1,49 +1,78 @@
+using System.Collections.Generic;
+using System.IO;
 using Unity.VisualScripting;
 using UnityEngine;
-using Random = UnityEngine.Random;
 
 public class EnemySpawn : MonoBehaviour
 {
-    public float maxEnemySpawnDelay;
-    public float curEnemySpawnDelay;
+    public GameObject[] enemySpwanPoints;
+    private float _curEnemySpawnDelay;
 
-    public GameObject[] EnemySpwanPoints;
-    private string[] enemies;
+    private Manager _manager;
+    private float _nextEnemySpawnDelay;
+    private Player _player;
+    private int _spawnIndex;
 
-    private Manager manager;
-    private Player player;
+    private List<Spawn> _spawns;
 
     private void Awake()
     {
-        enemies = new[] { "enemyL", "enemyM", "enemyS" };
+        _manager = GetComponent<Manager>();
+        _player = _manager.player;
+        _spawns = new List<Spawn>();
 
-        manager = GetComponent<Manager>();
-        player = manager.player;
+        ReadSpawnFile();
     }
 
     private void Update()
     {
-        curEnemySpawnDelay += Time.deltaTime;
-        if (curEnemySpawnDelay >= maxEnemySpawnDelay) SpawnEnemy();
+        _curEnemySpawnDelay += Time.deltaTime;
+        if (_curEnemySpawnDelay >= _nextEnemySpawnDelay && _spawns.Count > _spawnIndex) SpawnEnemy();
+    }
+
+    private void ReadSpawnFile()
+    {
+        _spawns.Clear();
+        var textAsset = Resources.Load<TextAsset>("Stage0");
+        var reader = new StringReader(textAsset.text);
+
+        while (!reader.IsUnityNull())
+        {
+            var line = reader.ReadLine();
+
+            if (line == null) break;
+
+            var properties = line.Split(',');
+            var spawn = new Spawn();
+            spawn.delay = float.Parse(properties[0]);
+            spawn.type = properties[1];
+            spawn.point = int.Parse(properties[2]);
+
+            _spawns.Add(spawn);
+        }
+
+        reader.Close();
+        _spawnIndex = 0;
+        _nextEnemySpawnDelay = _spawns[_spawnIndex].delay;
     }
 
     private void SpawnEnemy()
     {
-        curEnemySpawnDelay = 0;
-        maxEnemySpawnDelay = Random.Range(0.5f, 3f);
-        var enemyIndex = Random.Range(0, enemies.Length);
-        var enemyPoint = EnemySpwanPoints[Random.Range(0, EnemySpwanPoints.Length)];
-
-        // var enemyObj = Instantiate(Enemies[enemyIndex], enemyPoint.transform.position, enemyPoint.transform.rotation);
-        var enemyObj = manager.objectManager.MakeObject(enemies[enemyIndex]);
+        _curEnemySpawnDelay = 0;
+        var spawn = _spawns[_spawnIndex];
+        var enemyPoint = enemySpwanPoints[spawn.point];
+        var enemyObj = _manager.objectManager.MakeObject("enemy" + spawn.type);
 
         if (enemyObj.IsUnityNull()) return;
         enemyObj.transform.position = enemyPoint.transform.position;
         enemyObj.transform.rotation = enemyPoint.transform.rotation;
 
-        enemyObj.GetComponent<Enemy>().manager = manager;
+        enemyObj.GetComponent<Enemy>().manager = _manager;
         enemyObj.GetComponent<Enemy>().SetVelocity(enemyPoint.name);
-        enemyObj.GetComponent<Enemy>().GetComponent<EnemyFiring>().player = player;
-        enemyObj.GetComponent<Enemy>().GetComponent<EnemyFiring>().objectManager = manager.objectManager;
+        enemyObj.GetComponent<Enemy>().GetComponent<EnemyFiring>().player = _player;
+        enemyObj.GetComponent<Enemy>().GetComponent<EnemyFiring>().objectManager = _manager.objectManager;
+
+        _spawnIndex++;
+        if (_spawns.Count > _spawnIndex) _nextEnemySpawnDelay = _spawns[_spawnIndex].delay;
     }
 }
