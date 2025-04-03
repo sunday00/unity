dbe0a991b5a4118946dcf371e21621b0ee5facae
sunday00 <kind65505256@gmail.com>
2025-04-03 23:03:15 +0900

    yield

--- ytb-gm-prac-501/Assets/Ducks/Player/PlayerWeapon.cs
+++ ytb-gm-prac-501/Assets/Ducks/Player/PlayerWeapon.cs
@@ -26,6 +26,14 @@ namespace Ducks.Player
         private IEnumerator Swing()
         {
             yield return new WaitForSeconds(0.1f);
+            meleeArea.enabled = true;
+            trailEffect.enabled = true;
+
+            yield return new WaitForSeconds(0.3f);
+            meleeArea.enabled = false;
+
+            yield return new WaitForSeconds(0.3f);
+            trailEffect.enabled = false;
 
             yield break;
         }
