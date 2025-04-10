a5d5d8c109dd51ecf3edcaf1615cfdb6f9c8570c
sunday00 <kind65505256@gmail.com>
2025-04-10 23:14:46 +0900

    mass

--- ytb-gm-prac-501/Assets/Ducks/Interactival/Items/GrenadeBoom.cs
+++ ytb-gm-prac-501/Assets/Ducks/Interactival/Items/GrenadeBoom.cs
@@ -1,8 +1,28 @@
+using System.Collections;
 using UnityEngine;
 
 namespace Ducks.Interactival.Items
 {
     public class GrenadeBoom : MonoBehaviour
     {
+        public GameObject meshObj;
+        public GameObject effectObj;
+        public Rigidbody rb;
+
+        private void Start()
+        {
+            StartCoroutine(Explode());
+        }
+
+        private IEnumerator Explode()
+        {
+            yield return new WaitForSeconds(2f);
+
+            rb.linearVelocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+
+            meshObj.SetActive(false);
+            effectObj.SetActive(true);
+        }
     }
 }
