d35c6d0e1f0364571866978c493848710f31cf49
sunday00 <kind65505256@gmail.com>
2025-04-23 01:23:02 +0900

    shop

---
+++ ytb-gm-prac-501/Assets/Ducks/Interactival/Shop/Shop.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace Ducks.Interactival.Shop
+{
+    public class Shop : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        private void Start()
+        {
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+        }
+    }
+}
