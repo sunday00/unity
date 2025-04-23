d09a0d785f07331bdbd608919bee74b27a23bdfa
sunday00 <kind65505256@gmail.com>
2025-04-24 00:53:31 +0900

    shop script

--- ytb-gm-prac-501/Assets/Ducks/Interactival/Shop/Shop.cs
+++ ytb-gm-prac-501/Assets/Ducks/Interactival/Shop/Shop.cs
@@ -1,17 +1,26 @@
+using Ducks.Player;
 using UnityEngine;
 
 namespace Ducks.Interactival.Shop
 {
     public class Shop : MonoBehaviour
     {
+        public Animator animator;
+        public RectTransform ui;
+        private PlayerManager _player;
+
         // Start is called once before the first execution of Update after the MonoBehaviour is created
-        private void Start()
+        private void Enter(PlayerManager player)
         {
+            _player = player;
+            ui.anchoredPosition = Vector3.zero;
         }
 
         // Update is called once per frame
-        private void Update()
+        private void Exit()
         {
+            animator.SetTrigger("DoHello");
+            ui.anchoredPosition = Vector3.down * 1000;
         }
     }
 }
