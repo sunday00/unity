65394678f5d789d8d57dae5ccb817cdae8031440
sunday00 <kind65505256@gmail.com>
2025-04-26 02:45:21 +0900

    manager variables

--- ytb-gm-prac-501/Assets/Ducks/GameManager.cs
+++ ytb-gm-prac-501/Assets/Ducks/GameManager.cs
@@ -1,6 +1,7 @@
 using Ducks.Interactival.Enemies;
 using Ducks.Player;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Ducks
 {
@@ -20,5 +21,28 @@ namespace Ducks
         public int enemyCntA;
         public int enemyCntB;
         public int enemyCntC;
+
+        public GameObject menuPanel;
+        public GameObject mainPanel;
+        public Text maxScoreText;
+
+        public Text scoreText;
+        public Text stageText;
+        public Text playtimeText;
+
+        public Text playerHealthText;
+        public Text playerAmmoText;
+        public Text playerCoinText;
+
+        public Image weapon1Img;
+        public Image weapon2Img;
+        public Image weaponGImg;
+
+        public Text enemyAText;
+        public Text enemyBText;
+        public Text enemyCText;
+
+        public RectTransform bossHealthGroup;
+        public RectTransform bossHealthBar;
     }
 }
