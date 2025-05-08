79cb8fb3ed45cabd8a7b55c74fcb4b39b1654582
sunday00 <kind65505256@gmail.com>
2025-05-09 00:00:15 +0900

    drag drop

--- ytb-gm-prac-601/Assets/Duck/Player/Dongle.cs
+++ ytb-gm-prac-601/Assets/Duck/Player/Dongle.cs
@@ -5,17 +5,41 @@ namespace Duck.Player
     public class Dongle : MonoBehaviour
     {
         public Camera mainCamera;
+        public bool isDrag;
+        private Rigidbody2D _rb;
 
-        private void Start()
+
+        private void Awake()
         {
+            _rb = GetComponent<Rigidbody2D>();
         }
 
         private void Update()
         {
+            if (!isDrag) return;
+
             // Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             Vector2 mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
 
-            transform.position = Vector2.Lerp(transform.position, mousePosition, 0.1f);
+            var leftBorder = -3.9f + transform.localScale.x / 2f;
+            var rightBorder = 3.9f - transform.localScale.x / 2f;
+
+            mousePosition.x = mousePosition.x < leftBorder ? leftBorder :
+                mousePosition.x > rightBorder ? rightBorder : mousePosition.x;
+            mousePosition.y = 8f;
+
+            transform.position = Vector2.Lerp(transform.position, mousePosition, 0.2f);
+        }
+
+        public void Drag()
+        {
+            isDrag = true;
+        }
+
+        public void Drop()
+        {
+            isDrag = false;
+            _rb.simulated = true;
         }
     }
 }
