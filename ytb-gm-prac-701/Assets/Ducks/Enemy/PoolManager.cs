c533774ab73db7a0d4fccd7435566c624fe01e5e
sunday00 <kind65505256@gmail.com>
2025-06-17 01:34:53 +0900

    pool

--- ytb-gm-prac-701/Assets/Ducks/Enemy/PoolManager.cs
+++ ytb-gm-prac-701/Assets/Ducks/Enemy/PoolManager.cs
@@ -14,5 +14,27 @@ namespace Ducks.Enemy
 
             for (var i = 0; i < pools.Length; i++) pools[i] = new List<GameObject>();
         }
+
+        public GameObject Get(int index)
+        {
+            GameObject select = null;
+
+            foreach (var item in pools[index])
+                if (!item.activeSelf)
+                {
+                    select = item;
+                    select.SetActive(true);
+
+                    break;
+                }
+
+            if (!select)
+            {
+                select = Instantiate(prefabs[index], transform);
+                pools[index].Add(select);
+            }
+
+            return select;
+        }
     }
 }
