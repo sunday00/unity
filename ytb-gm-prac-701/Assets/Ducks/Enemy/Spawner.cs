8469e008ce88772fb7622f93b3ae4c90d4e1217b
sunday00 <kind65505256@gmail.com>
2025-06-24 00:34:57 +0900

    spawn

--- ytb-gm-prac-701/Assets/Ducks/Enemy/Spawner.cs
+++ ytb-gm-prac-701/Assets/Ducks/Enemy/Spawner.cs
@@ -4,9 +4,31 @@ namespace Ducks.Enemy
 {
     public class Spawner : MonoBehaviour
     {
+        public Transform[] spawnPoints;
+
+        private float _timer;
+
+        private void Awake()
+        {
+            spawnPoints = GetComponentsInChildren<Transform>();
+        }
+
         private void Update()
         {
-            if (Input.GetButtonDown("Jump")) GameManager.Instance.pool.Get(0);
+            _timer += Time.deltaTime;
+
+            if (_timer > 0.2f)
+            {
+                _timer = 0;
+                Spawn();
+            }
+        }
+
+        private void Spawn()
+        {
+            var enemy = GameManager.Instance.pool.Get(Random.Range(0, 2));
+
+            enemy.transform.position = spawnPoints[Random.Range(1, spawnPoints.Length)].position;
         }
     }
 }
